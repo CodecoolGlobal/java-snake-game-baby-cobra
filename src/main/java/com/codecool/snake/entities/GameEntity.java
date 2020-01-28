32bd1424aa6d4ed2da1536ceccceeecb307d049b
sandormatyas <sandormatyas7@gmail.com>
2020-01-28 14:26:12 +0100

    change outofbounds to count with png width and height

--- src/main/java/com/codecool/snake/entities/GameEntity.java
+++ src/main/java/com/codecool/snake/entities/GameEntity.java
@@ -26,8 +26,8 @@ public abstract class GameEntity extends ImageView {
     }
 
     public boolean isOutOfBounds() {
-        if (getX() > Globals.WINDOW_WIDTH || getX() < 0 ||
-            getY() > Globals.WINDOW_HEIGHT || getY() < 0) {
+        if (getX() > Globals.WINDOW_WIDTH - this.getImage().getWidth()  || getX() < 0 ||
+            getY() > Globals.WINDOW_HEIGHT - this.getImage().getHeight() || getY() < 0) {
             return true;
         }
         return false;
